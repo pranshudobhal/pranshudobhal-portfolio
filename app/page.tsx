9333fbd450431809863cd84418aa7266c4f0d858
Pranshu Dobhal <pranshu.dobhal@gmail.com>
2024-02-23 00:05:57 +0530

    feat: setup basic nextjs project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className='grid min-h-screen place-items-center'>
+      <h1 className='text-6xl'>Pranshu Dobhal</h1>
+    </main>
+  )
+}
