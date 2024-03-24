535b8057d3ea222bc3860f7f01c266844d23a2cb
Pranshu Dobhal <pranshu.dobhal@gmail.com>
2024-03-24 21:50:36 +0100

    feat: add redirect for blog

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/blog',
+        destination: process.env.BLOG_URL,
+      },
+      {
+        source: '/blog/:path*',
+        destination: `${process.env.BLOG_URL}/:path*`,
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
